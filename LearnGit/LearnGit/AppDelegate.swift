db211fadd0fe7a9ef6369410d374c92131f30223
zhangcy <zcy0708@foxmail.com>
2020-09-25 11:25:16 +0800

    first commit

---
+++ LearnGit/LearnGit/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  LearnGit
+//
+//  Created by 张辰宇 on 2020/9/25.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
